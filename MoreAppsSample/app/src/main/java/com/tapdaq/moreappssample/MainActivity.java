c4915c8fe2dd4c522041f917751bedd1c893b4cc
Dominic Roberts <dom.roberts@tapdaq.com>
2018-11-05 15:18:30 +0000

    Revamped MoreApps sample. - Added initialise button - Show button auto toggles on availability - Added logger - Added debugger button

--- MoreAppsSample/app/src/main/java/com/tapdaq/moreappssample/MainActivity.java
+++ MoreAppsSample/app/src/main/java/com/tapdaq/moreappssample/MainActivity.java
@@ -3,61 +3,52 @@ package com.tapdaq.moreappssample;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
+import android.widget.Button;
+import android.widget.ListView;
 import com.tapdaq.adapters.tapdaq.moreapps.TMMoreAppsConfig;
-import com.tapdaq.adapters.tapdaq.moreapps.TMMoreAppsListener;
 import com.tapdaq.sdk.CreativeType;
 import com.tapdaq.sdk.Tapdaq;
 import com.tapdaq.sdk.TapdaqConfig;
 import com.tapdaq.sdk.TapdaqPlacement;
 import com.tapdaq.sdk.common.TMAdError;
+import com.tapdaq.sdk.debug.TMDebugAdapter;
+import com.tapdaq.sdk.helpers.TLog;
+import com.tapdaq.sdk.helpers.TLogLevel;
 import com.tapdaq.sdk.listeners.TMAdListener;
 import com.tapdaq.sdk.listeners.TMInitListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "TAPDAQ";
 
-    private MoreAppsListener mMoreAppsListener = new MoreAppsListener();
+    private String mAppId = "";
+    private String mClientKey = "";
+
+    private TMDebugAdapter mLogListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.load_moreAppsBtn).setOnClickListener(new OnClickLoadMoreApps());
-        findViewById(R.id.show_moreapps_btn).setOnClickListener(new OnClickShowMoreApps());
+        TLog.setLoggingLevel(TLogLevel.DEBUG);
 
-        String mApplicationId = "";
-        String mClientKey = "";
+        //Get Show Buttons from layout
+        findViewById(R.id.init_btn).setOnClickListener(new ClickInitialise());
+        findViewById(R.id.show_debug_btn).setOnClickListener(new ClickShowDebug());
 
-        List<TapdaqPlacement> placements = new ArrayList<>();
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-1"));
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-2"));
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-3"));
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-4"));
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-5"));
-        placements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-backfill"));
+        findViewById(R.id.load_btn).setOnClickListener(new ClickLoadAd());
+        findViewById(R.id.show_btn).setOnClickListener(new ClickShowAd());
 
-        TapdaqConfig config = new TapdaqConfig();
-        config.withPlacementTagSupport(placements.toArray(new TapdaqPlacement[placements.size()]));
-
-        Tapdaq.getInstance().initialize(this, mApplicationId, mClientKey, config, new TMInitListener() {
-            @Override
-            public void didInitialise() {
-                super.didInitialise();
-                findViewById(R.id.load_moreAppsBtn).setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void didFailToInitialise(TMAdError error) {
-                super.didFailToInitialise(error);
-            }
-        });
+        //Logger
+        mLogListAdapter = new TMDebugAdapter(this, new ArrayList<String>());
+        ((ListView)findViewById(R.id.log_listview)).setAdapter(mLogListAdapter);
     }
 
     private TMMoreAppsConfig getCustomMoreAppsConfig() {
@@ -78,53 +69,151 @@ public class MainActivity extends AppCompatActivity {
         return config;
     }
 
-    private class OnClickLoadMoreApps implements View.OnClickListener
-    {
-        @Override
-        public void onClick(View v) {
-            Tapdaq.getInstance().loadMoreApps(MainActivity.this, getCustomMoreAppsConfig(), mMoreAppsListener);
+    private void updateView() {
+        if (Tapdaq.getInstance().isMoreAppsReady(this)) {
+            findViewById(R.id.show_btn).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.show_btn).setVisibility(View.GONE);
         }
     }
 
-    private class OnClickShowMoreApps implements View.OnClickListener
+    private class ClickInitialise implements View.OnClickListener
     {
         @Override
+        public void onClick(View view) {
+            //Set Placements & Ad Types
+            List<TapdaqPlacement> enabledPlacements = new ArrayList<>();
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-1"));
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-2"));
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-3"));
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-4"));
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-5"));
+            enabledPlacements.add(TapdaqPlacement.createPlacement(Arrays.asList(CreativeType.SQUARE_MEDIUM), "tray-position-backfill"));
+
+            //Configuration
+            TapdaqConfig config = new TapdaqConfig();
+            config.setAutoReloadAds(true);
+            config.withPlacementTagSupport(enabledPlacements.toArray(new TapdaqPlacement[enabledPlacements.size()]));
+
+            //Initialise app
+            mLogListAdapter.insert("Click Initialise", 0);
+            ((Button)findViewById(R.id.init_btn)).setText(R.string.initialising_label);
+            findViewById(R.id.init_btn).setEnabled(false);
+            Tapdaq.getInstance().initialize(MainActivity.this, mAppId, mClientKey, config, new InitListener());
+        }
+    }
+
+    private class ClickShowDebug implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view) {
+            /* Start Test Activity
+             * Params Activity (Current Activity)
+             */
+            Tapdaq.getInstance().startTestActivity(MainActivity.this);
+        }
+    }
+
+    private class ClickLoadAd implements View.OnClickListener {
+
+        @Override
         public void onClick(View v) {
+            Tapdaq.getInstance().loadMoreApps(MainActivity.this, getCustomMoreAppsConfig(), new AdListener());
+        }
+    }
+
+    private class ClickShowAd implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
             if (Tapdaq.getInstance().isMoreAppsReady(MainActivity.this)) {
-                Tapdaq.getInstance().showMoreApps(MainActivity.this, mMoreAppsListener);
+                Tapdaq.getInstance().showMoreApps(MainActivity.this, new AdListener());
             }
+
+            updateView();
         }
     }
 
-    private class MoreAppsListener extends TMAdListener {
+    private class InitListener extends TMInitListener
+    {
+        @Override
+        public void didInitialise() {
+            super.didInitialise();
+            findViewById(R.id.init_btn).setVisibility(View.GONE);
+            findViewById(R.id.content_view).setVisibility(View.VISIBLE);
+            Log.i(TAG, "didInitialise");
+            mLogListAdapter.insert("didInitialise", 0);
+        }
+
+        @Override
+        public void didFailToInitialise(TMAdError error) {
+            super.didFailToInitialise(error);
+            ((Button)findViewById(R.id.init_btn)).setText(R.string.initialise_label);
+
+            String str = String.format(Locale.ENGLISH, "didFailToInitialise: %d - %s", error.getErrorCode(), error.getErrorMessage());
+
+            for (String key : error.getSubErrors().keySet()) {
+                TMAdError value = error.getSubErrors().get(key);
+                String subError = String.format(Locale.ENGLISH, "%s - %d: %s", key,  value.getErrorCode(), value.getErrorMessage());
+                str = str.concat("\n ");
+                str = str.concat(subError);
+            }
+
+            Log.i(TAG, str);
+            mLogListAdapter.insert(str, 0);
+            findViewById(R.id.init_btn).setEnabled(true);
+        }
+    }
+
+
+    public class AdListener extends TMAdListener {
         @Override
         public void didLoad() {
-            super.didLoad();
-
-            if (Tapdaq.getInstance().isMoreAppsReady(MainActivity.this)) {
-                findViewById(R.id.show_moreapps_btn).setVisibility(View.VISIBLE);
-            }
+            updateView();
+            Log.i(TAG, "didLoad");
+            mLogListAdapter.insert("didLoad", 0);
         }
 
         @Override
         public void didFailToLoad(TMAdError error) {
-            super.didFailToLoad(error);
-            Toast.makeText(MainActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            updateView();
+
+            String str = String.format(Locale.ENGLISH, "didFailToLoad: %d - %s", error.getErrorCode(), error.getErrorMessage());
+
+            for (String key : error.getSubErrors().keySet()) {
+                TMAdError value = error.getSubErrors().get(key);
+                String subError = String.format(Locale.ENGLISH, "%s - %d: %s", key,  value.getErrorCode(), value.getErrorMessage());
+                str = str.concat("\n ");
+                str = str.concat(subError);
+            }
+
+            Log.i(TAG, str);
+            mLogListAdapter.insert(str, 0);
         }
 
         @Override
         public void willDisplay() {
-            super.willDisplay();
+            Log.i(TAG, "willDisplay");
+            mLogListAdapter.insert("willDisplay", 0);
         }
 
         @Override
         public void didDisplay() {
-            super.didDisplay();
+            Log.i(TAG, "didDisplay");
+            mLogListAdapter.insert("didDisplay", 0);
+        }
+
+        @Override
+        public void didClick() {
+            Log.i(TAG, "didClick");
+            mLogListAdapter.insert("didClick", 0);
         }
 
         @Override
         public void didClose() {
-            super.didClose();
+            updateView();
+            Log.i(TAG, "didClose");
+            mLogListAdapter.insert("didClose", 0);
         }
     }
 }
